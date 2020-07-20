d89972b69e40a24d87f583512b9fd17d44ad26e0
Melinda Diaz <melindadiaz@me.com>
2020-07-20 22:31:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,9 @@ def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
   
-  
+  array.map do |num|
+    num**2
+  end
   
   
 end
